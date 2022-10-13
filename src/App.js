5ed1f8f2ef38903aacb0377930efe4633ecaf99a
AndrewKohn <87670203+itsRunic@users.noreply.github.com>
2022-10-12 21:26:39 -0500

    added new components to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import classes from './App.module.css';
+import Footer from './components/Footer/Footer';
 import MainHeader from './components/MainHeader/MainHeader';
 import PreviewSection from './components/PreviewSection/PreviewSection';
+import Testimonials from './components/Testimonials/Testimonials';
 
 function App() {
   return (
     <main className={classes.main}>
       <MainHeader />
       <PreviewSection />
+      <Testimonials />
+      <Footer />
     </main>
   );
 }
