fee4f5114ec918d26a01670f31964d545a664c20
itsRunic <87670203+itsRunic@users.noreply.github.com>
2022-10-16 17:25:02 -0500

    Added social icon svg

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,23 @@
+import facebookIcon from '../../assets/images/socials/facebook.svg';
+import twitterIcon from '../../assets/images/socials/twitter.svg';
+import instagramIcon from '../../assets/images/socials/instagram.svg';
+
 import classes from './Footer.module.css';
 
 const Footer = props => {
-  return <footer className={classes.footer}></footer>;
+  return (
+    <footer className={classes.footer}>
+      <a href="#">
+        <img src={facebookIcon} />
+      </a>
+      <a href="#">
+        <img src={twitterIcon} />
+      </a>
+      <a href="#">
+        <img src={instagramIcon} />
+      </a>
+    </footer>
+  );
 };
 
 export default Footer;
